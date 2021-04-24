328e187e48fa7a14c99fe01b4db8a350f7eb904f
muzammal kazmi <muzammalkazmi@gmail.com>
2021-04-24 15:03:41 +0500

    updated hold button and toggle player

--- LudoGame/script.js
+++ LudoGame/script.js
@@ -4,30 +4,47 @@ const holdTwo = document.getElementById('score--1')
 var runningOne = document.getElementById('current--0')
 var runningTwo = document.getElementById('current--1')
 var rolBtn = document.getElementById('rolBtn')
+const holdBtn = document.querySelector('.holdBtn')
 const showDice = document.querySelector('.dice')
 rolBtn.addEventListener('click', init)
-var roundScore, activePlayer;
-roundScore = 1
-    // var activePlayer = 1
+var roundScore, activePlayer, scores;
+roundScore = 0;
+activePlayer = 0;
+scores = [0, 0]
 document.getElementById('score-0').textContent = '0'
 document.getElementById('current-0').textContent = '0'
-document.getElementById('score--1').textContent = '0'
+document.getElementById('score-1').textContent = '0'
 document.getElementById('current-1').textContent = '0'
+document.querySelector('.dice').style.display = 'none'
 
 function init() {
     var digit = Math.floor(Math.random() * 6) + 1
     console.log(digit)
+        //show the dice
     showDice.src = `dice-${digit}.png`;
-    //if score is not equal to 1
+    document.querySelector('.dice').style.display = 'block'
+        //if score is not equal to 1
     if (digit !== 1) {
         //add the score 
-        console.log(digit)
         roundScore += digit
-        console.log(roundScore)
-        document.getElementById('current-0').textContent = roundScore
+        document.getElementById('current-' + activePlayer).textContent = roundScore
     } else {
-
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        document.getElementById('current-0').textContent = 0
+        document.getElementById('current-1').textContent = 0
+        roundScore = 0
     }
 
 }
+
+holdBtn.addEventListener('click', function() {
+    //update the global score as per current score
+    scores[activePlayer] += roundScore
+    document.getElementById('score-' + activePlayer).textContent = scores[activePlayer]
+    roundScore = 0;
+    document.getElementById('current-' + activePlayer).textContent = 0;
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
+
+    //toggle players
+})
 init()
